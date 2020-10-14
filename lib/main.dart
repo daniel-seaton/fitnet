b179272eb751ac16ec4d40b160e31cc97083cd21
Daniel Seaton <seatondanny@gmail.com>
2020-10-13 22:45:29 -0500

    Fixing some async errors that were showing up on launch

--- lib/main.dart
+++ lib/main.dart
@@ -16,34 +16,27 @@ class Fitnet extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: _firebaseInit,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return MaterialApp(
-              title: 'Flutter Demo',
-              theme: ThemeData(
-                // This is the theme of your application.
-                //
-                // Try running your application with "flutter run". You'll see the
-                // application has a blue toolbar. Then, without quitting the app, try
-                // changing the primarySwatch below to Colors.green and then invoke
-                // "hot reload" (press "r" in the console where you ran "flutter run",
-                // or simply save your changes to "hot reload" in a Flutter IDE).
-                // Notice that the counter didn't reset back to zero; the application
-                // is not restarted.
-                primarySwatch: Colors.blue,
-                // This makes the visual density adapt to the platform that you run
-                // the app on. For desktop platforms, the controls will be smaller and
-                // closer together (more dense) than on mobile platforms.
-                visualDensity: VisualDensity.adaptivePlatformDensity,
-              ),
-              home: MyHomePage(),
-            );
-          } else {
-            return Center(child: Text('Loading'));
-          }
-        });
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Fitnet'),
+        ),
+        body: FutureBuilder(
+            future: _firebaseInit,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return MyHomePage();
+              } else {
+                return Center(child: Text('Loading'));
+              }
+            }),
+      ),
+    );
   }
 }
 
@@ -51,26 +44,20 @@ class MyHomePage extends StatelessWidget {
   final AuthService auth = AuthService();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Fitnet'),
-      ),
-      body: Center(
-        child: StreamProvider.value(
-          value: auth.getLoggedInUser(),
-          child: Consumer<AppUser>(builder: (context, user, _) {
-            List<Widget> children = [];
-            if (user != null) {
-              children.add(TempScreen());
-            } else {
-              children.add(LoginScreen());
-            }
+    return Center(
+      child: StreamProvider.value(
+        value: auth.getLoggedInUser(),
+        child: Consumer<AppUser>(builder: (context, user, _) {
+          List<Widget> children = [];
+          if (user != null) {
+            children.add(TempScreen());
+          } else {
+            children.add(LoginScreen());
+          }
 
-            return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: children);
-          }),
-        ),
+          return Column(
+              mainAxisAlignment: MainAxisAlignment.center, children: children);
+        }),
       ),
     );
   }
