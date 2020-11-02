97addbd0b8a282801d4342e5296abaec9ac22779
Daniel Seaton <seatondanny@gmail.com>
2020-11-02 08:42:28 -0600

    Allowing steps to be deleted on right swipe

--- lib/src/app/workout/editWorkoutScreen/formFields/stepListField.dart
+++ lib/src/app/workout/editWorkoutScreen/formFields/stepListField.dart
@@ -2,6 +2,7 @@ import 'package:fitnet/models/format.dart';
 import 'package:fitnet/models/workoutStep.dart';
 import 'package:fitnet/src/app/workout/editWorkoutScreen/editChangeNotifier.dart';
 import 'package:fitnet/src/app/workout/editWorkoutScreen/formFields/editStep/editStepModal.dart';
+import 'package:fitnet/src/app/workout/editWorkoutScreen/formFields/stepListItem.dart';
 import 'package:fitnet/src/app/workout/editWorkoutScreen/workoutChangeNotifier.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -16,104 +17,69 @@ class StepListField extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-      child: Consumer<EditChangeNotifier>(
-        builder: (_, editNotifier, __) => Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width - 40,
-              child:
-                  Text('Steps:', style: Theme.of(context).textTheme.bodyText1),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey, width: 0.5)),
-              height: MediaQuery.of(context).size.height / 2.1,
-              width: MediaQuery.of(context).size.width - 40,
-              child: Consumer<WorkoutChangeNotifier>(
-                builder: (_, workoutNotifier, __) => ReorderableListView(
-                  onReorder: (x, y) {
-                    if (!editNotifier.isEdit) return;
-                    if (x != y) {
-                      WorkoutStep step = workoutNotifier.removeStep(x);
-                      workoutNotifier.addStep(step, index: y > x ? y - 1 : y);
-                    }
-                  },
-                  children: List.generate(
-                    steps.length,
-                    (index) {
-                      WorkoutStep step = steps[index];
-                      return InkWell(
-                        key: Key(step.exercise.name),
-                        onTap: () => showStepModal(
-                          context,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width - 40,
+            child: Text('Steps:', style: Theme.of(context).textTheme.bodyText1),
+          ),
+          Container(
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey, width: 0.5)),
+            height: MediaQuery.of(context).size.height / 2.1,
+            width: MediaQuery.of(context).size.width - 40,
+            child: Consumer2<EditChangeNotifier, WorkoutChangeNotifier>(
+              builder: (_, editNotifier, workoutNotifier, __) =>
+                  ReorderableListView(
+                onReorder: (x, y) {
+                  if (!editNotifier.isEdit) return;
+                  if (x != y) {
+                    WorkoutStep step = workoutNotifier.removeStep(x);
+                    workoutNotifier.addStep(step, index: y > x ? y - 1 : y);
+                  }
+                },
+                children: List.generate(
+                  steps.length,
+                  (index) {
+                    return StepListItem(
+                      key: Key(steps[index].exercise.name),
+                      step: steps[index],
+                      showStepModal: (step, isEdit) => showStepModal(context,
                           step: step,
-                          isEdit: editNotifier.isEdit,
+                          isEdit: isEdit,
                           onSave: (WorkoutStep step) => workoutNotifier
-                              .addStep(step, index: index, isEdit: true),
-                        ),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            border: Border(
-                              bottom:
-                                  BorderSide(color: Colors.grey, width: 0.5),
-                            ),
-                          ),
-                          padding: EdgeInsets.all(10),
-                          height: 60,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(step.exercise.name,
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .bodyText1),
-                                  Text(step.format.displayValue,
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .bodyText2),
-                                ],
-                              ),
-                              editNotifier.isEdit
-                                  ? IconButton(
-                                      icon: Icon(Icons.reorder),
-                                      onPressed: null)
-                                  : Container(width: 0, height: 0)
-                            ],
-                          ),
-                        ),
-                      );
-                    },
-                  ),
+                              .addStep(step, index: index, isEdit: true)),
+                      onDismissed: () => workoutNotifier.removeStep(index),
+                    );
+                  },
                 ),
               ),
             ),
-            editNotifier.isEdit
-                ? Consumer<WorkoutChangeNotifier>(
-                    builder: (_, workoutNotifier, __) => OutlineButton(
-                      key: Key('addStepButton'),
-                      color: Colors.grey,
-                      onPressed: () => showStepModal(
-                        context,
-                        isEdit: true,
-                        onSave: (WorkoutStep step) =>
-                            workoutNotifier.addStep(step),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(Icons.add, color: Colors.grey),
-                          Text('Add Step', style: TextStyle(color: Colors.grey))
-                        ],
-                      ),
+          ),
+          Consumer2<EditChangeNotifier, WorkoutChangeNotifier>(
+            builder: (_, editNotifier, workoutNotifier, __) => editNotifier
+                    .isEdit
+                ? OutlineButton(
+                    key: Key('addStepButton'),
+                    color: Colors.grey,
+                    onPressed: () => showStepModal(
+                      context,
+                      isEdit: true,
+                      onSave: (WorkoutStep step) =>
+                          workoutNotifier.addStep(step),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.add, color: Colors.grey),
+                        Text('Add Step', style: TextStyle(color: Colors.grey))
+                      ],
                     ),
                   )
-                : Container(key: Key('emptyContainer'), height: 0)
-          ],
-        ),
+                : Container(key: Key('emptyContainer'), height: 0),
+          )
+        ],
       ),
     );
   }
---
+++ lib/src/app/workout/editWorkoutScreen/formFields/stepListItem.dart
@@ -0,0 +1,64 @@
+import 'package:fitnet/models/workoutStep.dart';
+import 'package:fitnet/src/app/workout/editWorkoutScreen/editChangeNotifier.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class StepListItem extends StatelessWidget {
+  final Key key;
+  final WorkoutStep step;
+  final Function showStepModal;
+  final Function onDismissed;
+
+  StepListItem(
+      {@required this.key,
+      @required this.step,
+      @required this.showStepModal,
+      @required this.onDismissed});
+
+  @override
+  Widget build(BuildContext context) {
+    EditChangeNotifier editNotifier = Provider.of<EditChangeNotifier>(context);
+    return Dismissible(
+      key: key,
+      direction: editNotifier.isEdit ? DismissDirection.horizontal : null,
+      background: Container(
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.fromLTRB(0, 0, 25, 0),
+        color: Colors.red,
+        child: Icon(Icons.delete, color: Colors.white),
+      ),
+      onDismissed: (dir) {
+        if (editNotifier.isEdit) onDismissed();
+      },
+      child: InkWell(
+        onTap: () => showStepModal(step, editNotifier.isEdit),
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(color: Colors.grey, width: 0.5),
+            ),
+          ),
+          padding: EdgeInsets.all(10),
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(step.exercise.name,
+                      style: Theme.of(context).textTheme.bodyText1),
+                  Text(step.format.displayValue,
+                      style: Theme.of(context).textTheme.bodyText2),
+                ],
+              ),
+              editNotifier.isEdit
+                  ? IconButton(icon: Icon(Icons.reorder), onPressed: null)
+                  : Container(width: 0, height: 0)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
